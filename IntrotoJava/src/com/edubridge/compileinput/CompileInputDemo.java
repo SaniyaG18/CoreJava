3ec4a07e50f904b913e76dee42d839be9f3a5334
User <User@lenovo-PC-18>
2024-01-18 13:28:51 +0530

    First Commit

---
+++ IntrotoJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,26 @@
+package com.edubridge.compileinput;
+//Program to demonstrate data types
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello world");
+		byte num = 127;
+		short num1 =  9999;
+		int num2 = 214217845;
+		long num3 = 966566669;
+		char a = 'D';
+		float num4 = 53.5f;
+		double num5 = 5.555d;
+		String str = "Saniya";
+		System.out.println(num);
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+		System.out.println(a);
+		System.out.println(str);
+	}
+
+}
